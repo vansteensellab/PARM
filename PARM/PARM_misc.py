84d0ab5d18b6ba664fc14fd481c841cc0f8fd163
vhfsantos <vfranceschini@gmail.com>
2024-07-18 15:02:50 +0200

    Add preprint citation, comment on functions

--- PARM/PARM_misc.py
+++ PARM/PARM_misc.py
@@ -1,5 +1,23 @@
 from .version import __version__
 
-def log(message):
+def log(message: str):
+    """
+    Simple function to print a log message. 
+    This writes the message to the console with a version number.
+    
+    Parameters
+    ----------
+    message : str
+        The message to print to the console.
+    
+    Returns
+    -------
+    None
+    
+    Examples
+    --------
+    >>> log("This is a message")
+    [PARM v0.1.0] This is a message
+    """
     v = 'PARM v' + __version__
     print(f"[{v}] {message}", flush=True)

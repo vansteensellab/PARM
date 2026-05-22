1b22c0920b558b9d68a3e90fe23091b59862c4b8
Vinícius Franceschini-Santos <v.franceschini@nki.nl>
2026-05-22 13:09:48 +0000

    fix import issue

--- PARM/PARM_model_evaluation.py
+++ PARM/PARM_model_evaluation.py
@@ -196,6 +196,10 @@ def insert_motifs_in_random_sequences(
 def plot_motif_correlation(motif_correlation_across_random_sequences, database_id, motif_id, cell_type, model_id, output_directory):    
         ##Also save the correlations in dataframe that contains id of the motif and  the correlation value
         import pandas as pd
+        import os
+        import numpy as np
+        import matplotlib.pyplot as plt
+        import seaborn as sns
         
         for cell in cell_type.split("__"):
             motif_id_cell = motif_id[cell]
